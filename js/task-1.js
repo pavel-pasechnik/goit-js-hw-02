5609d5deb2034129f93a2a16364090675039e2b6
Pavel <Pavel.Pasechnik@gmail.com>
2023-11-03 15:31:03 +0200

    adit mistake

--- js/task-1.js
+++ js/task-1.js
@@ -2,7 +2,7 @@
 
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
   const totalPrice = quantity * pricePerDroid;
-  if (quantity * pricePerDroid > customerCredits) {
+  if (totalPrice > customerCredits) {
     return "Insufficient funds!";
   } else {
     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
